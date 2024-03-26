4a609cf99898532dadf5da036da65d43ca35e761
Ákos Klobetz <klobetz@gmail.com>
2024-03-26 18:32:00 +0100

    A context létrehozása

---
+++ BlazorApp_DB_Entity/Data/MySql_DB_Context.cs
@@ -0,0 +1,11 @@
+﻿using BlazorApp_DB_Entity.Modell;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorApp_DB_Entity.Data
+{
+    public class MySql_DB_Context : DbContext
+    {
+        public MySql_DB_Context(DbContextOptions<MySql_DB_Context> options) : base(options) { }
+        public DbSet<User> User { get; set; } = default;
+    }
+}
