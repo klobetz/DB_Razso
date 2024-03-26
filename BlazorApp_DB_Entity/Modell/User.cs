26c32bf9c60203851c6f3e7f568602ff86251928
Ákos Klobetz <klobetz@gmail.com>
2024-03-26 18:09:52 +0100

    A modell létrehozása

---
+++ BlazorApp_DB_Entity/Modell/User.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorApp_DB_Entity.Modell
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string? Name { get; set; }
+        [Required]
+        public string? Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,20}$", ErrorMessage ="a jelszónak legalább 8 karakternek kell lennie!")]
+        public string? Password { get; set; }
+
+    }
+}
