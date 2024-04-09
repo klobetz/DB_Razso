dad70ed8a9e155158e8ccf0c34fb4cbec763a1ae
Ákos Klobetz <klobetz@gmail.com>
2024-04-09 16:41:34 +0200

    Valós idejű validáció elvégzése és egy új nuget csomag használata

--- BlazorApp_DB_Entity/Modell/User.cs
+++ BlazorApp_DB_Entity/Modell/User.cs
@@ -6,13 +6,18 @@ namespace BlazorApp_DB_Entity.Modell
     {
         [Key]
         public int Id { get; set; }
-        [Required]
+
+        [Required(ErrorMessage ="A mező kitöltése kötelező")]
         public string? Name { get; set; }
-        [Required]
+
+        [Required(ErrorMessage = "A mező kitöltése kötelező")]
         public string? Email { get; set; }
-        [Required]
+
+        [Required(ErrorMessage = "A mező kitöltése kötelező")]
+        [MinLength(8,ErrorMessage = "a jelszónak legalább 8 karakternek kell tartalmaznia")]
+        [MaxLength(20,ErrorMessage = "a jelszó maximum 20 karakter hosszú lehet")]
         [DataType(DataType.Password)]
-        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,20}$", ErrorMessage ="a jelszónak legalább 8 karakternek kell lennie!")]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,20}$", ErrorMessage ="A jeszónak: kisbetűt, nagybetűt és számot valamint speciális karaktert kell tartalmaznia")]
         public string? Password { get; set; }
 
     }
